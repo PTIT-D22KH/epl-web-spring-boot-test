adbce5172975c8beade26e99ebffbea07719f8fb
duongvu5 <vucongtuanduong5@gmail.com>
2025-02-08 13:48:06 +0700

    Add files via upload

---
+++ epl-app/src/main/java/vn/duongvct/test/epl_app/EplAppApplication.java
@@ -0,0 +1,13 @@
+package vn.duongvct.test.epl_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EplAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EplAppApplication.class, args);
+	}
+
+}
---
+++ epl-app/src/test/java/vn/duongvct/test/epl_app/EplAppApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.duongvct.test.epl_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
