4cbd116590a3630f1aec8468289ee9b2ef340195
duongvu5 <vucongtuanduong5@gmail.com>
2025-02-08 07:16:38 +0000

    update

--- init.sql
+++ init.sql
@@ -1,4 +1,11 @@
-CREATE USER 'test_user'@'%' IDENTIFIED BY 'superSecretPassword!123';
-GRANT ALL PRIVILEGES ON *.* TO 'test_user'@'%' WITH GRANT OPTION;
+-- Create the database
+CREATE DATABASE IF NOT EXISTS epl;
+
+-- Create the user
+CREATE USER IF NOT EXISTS 'test_user'@'%' IDENTIFIED BY 'superSecretPassword!123';
+
+-- Grant all privileges on the epl database to the user
+GRANT ALL PRIVILEGES ON epl.* TO 'test_user'@'%' WITH GRANT OPTION;
+
+-- Flush privileges to ensure that the changes take effect
 FLUSH PRIVILEGES;
-CREATE DATABASE epl;
