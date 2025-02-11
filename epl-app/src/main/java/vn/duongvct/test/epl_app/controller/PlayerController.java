98ecf27b3bea4ee4e80352a3fd6c6763e563cf8c
duongvu5 <vucongtuanduong5@gmail.com>
2025-02-11 02:13:46 +0000

    test

--- epl-app/src/main/java/vn/duongvct/test/epl_app/controller/PlayerController.java
+++ epl-app/src/main/java/vn/duongvct/test/epl_app/controller/PlayerController.java
@@ -1,38 +1,47 @@
-package vn.duongvct.test.epl_app.controller;
+// package vn.duongvct.test.epl_app.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import vn.duongvct.test.epl_app.domain.Player;
-import vn.duongvct.test.epl_app.domain.request.player.RequestCreatePlayerDTO;
-import vn.duongvct.test.epl_app.domain.response.player.ResponseCreatePlayerDTO;
-import vn.duongvct.test.epl_app.service.PlayerService;
-import vn.duongvct.test.epl_app.util.annotation.ApiMessage;
+// import com.turkraft.springfilter.boot.Filter;
 
-@RestController
-@RequestMapping("/api/v1")
-public class PlayerController {
-    private PlayerService playerService;
-    public PlayerController(PlayerService playerService) {
-        this.playerService = playerService;
-    }
+// import jakarta.validation.Valid;
+// import vn.duongvct.test.epl_app.domain.Player;
+// import vn.duongvct.test.epl_app.domain.request.player.RequestCreatePlayerDTO;
+// import vn.duongvct.test.epl_app.domain.response.player.ResponseCreatePlayerDTO;
+// import vn.duongvct.test.epl_app.service.PlayerService;
+// import vn.duongvct.test.epl_app.util.annotation.ApiMessage;
 
-    // @GetMapping("/players/{id}")
-    // public ResponseEntity<Void> showPlayer(@PathVariable Long id) {
+// import java.util.List;
+// import java.util.Optional;
 
-    // }
+// import org.springframework.data.domain.PageRequest;
+// import org.springframework.data.domain.Pageable;
+// import org.springframework.data.jpa.domain.Specification;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PutMapping;
 
-    @PostMapping("/players")
-    @ApiMessage("Create a new player")
-    public ResponseEntity<ResponseCreatePlayerDTO> createNewPlayer(@RequestBody RequestCreatePlayerDTO player) {
-        Player newPlayer = this.playerService.handleCreatePlayer(this.playerService.convertRequestCreatePlayerDTOtoPlayer(player));
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.playerService.convertPlayerToResponseCreatePlayerDTO(newPlayer));
-    }
+// @RestController
+// @RequestMapping("/api/v1")
 
-}
+// public class PlayerController {
+//     private final PlayerService playerService;
+
+//     public PlayerController(PlayerService playerService) {
+//         this.playerService = playerService;
+//     }
+
+//     @PostMapping("/players")
+//     @ApiMessage("Create a new player")
+//     public ResponseEntity<ResponseCreatePlayerDTO> createNewPlayer(@RequestBody RequestCreatePlayerDTO player) {
+//         Player newPlayer = this.playerService.handleCreatePlayer(this.playerService.convertRequestCreatePlayerDTOtoPlayer(player));
+//         return ResponseEntity.status(HttpStatus.CREATED).body(this.playerService.convertPlayerToResponseCreatePlayerDTO(newPlayer));
+//     }
+
+// }
--- epl-app/src/main/java/vn/duongvct/test/epl_app/domain/request/player/RequestCreatePlayerDTO.java
+++ epl-app/src/main/java/vn/duongvct/test/epl_app/domain/request/player/RequestCreatePlayerDTO.java
@@ -2,7 +2,9 @@ package vn.duongvct.test.epl_app.domain.request.player;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import vn.duongvct.test.epl_app.constant.GenderEnum;
 import vn.duongvct.test.epl_app.domain.ClubPlayer;
@@ -10,7 +12,10 @@ import vn.duongvct.test.epl_app.domain.PlayerPosition;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestCreatePlayerDTO {
+    
     private String name;
     private String nation;
     private int age;
