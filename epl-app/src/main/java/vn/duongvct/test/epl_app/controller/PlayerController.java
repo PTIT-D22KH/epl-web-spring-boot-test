292fea15165d4a1a958ceb13707a103402b934ab
duongvu4 <vucongtuanduong4@gmail.com>
2025-02-12 02:25:06 +0000

    update delete mapping playercontroller

--- epl-app/src/main/java/vn/duongvct/test/epl_app/controller/PlayerController.java
+++ epl-app/src/main/java/vn/duongvct/test/epl_app/controller/PlayerController.java
@@ -2,18 +2,26 @@ package vn.duongvct.test.epl_app.controller;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.turkraft.springfilter.boot.Filter;
+
 import jakarta.validation.Valid;
 import vn.duongvct.test.epl_app.domain.Player;
 import vn.duongvct.test.epl_app.domain.request.player.RequestCreatePlayerDTO;
 import vn.duongvct.test.epl_app.domain.request.player.RequestUpdatePlayerDTO;
+import vn.duongvct.test.epl_app.domain.response.ResultPaginationDTO;
 import vn.duongvct.test.epl_app.domain.response.player.ResponseCreatePlayerDTO;
 import vn.duongvct.test.epl_app.domain.response.player.ResponseUpdatePlayerDTO;
 import vn.duongvct.test.epl_app.service.PlayerService;
@@ -32,7 +40,7 @@ public class PlayerController {
 
     @PostMapping("/players")
     @ApiMessage("Create a new player")
-    public ResponseEntity<ResponseCreatePlayerDTO> createNewPlayer(@Valid @RequestBody RequestCreatePlayerDTO player) {
+    public ResponseEntity<ResponseCreatePlayerDTO> createNewPlayer(@RequestBody RequestCreatePlayerDTO player) {
         Player newPlayer = this.playerService.handleCreatePlayer(this.playerService.convertRequestCreatePlayerDTOtoPlayer(player));
         return ResponseEntity.status(HttpStatus.CREATED).body(this.playerService.convertPlayerToResponseCreatePlayerDTO(newPlayer));
     }
@@ -46,6 +54,31 @@ public class PlayerController {
         }
         Player updatedPlayer = this.playerService.handleUpdatePlayer(player.get(), playerDTO);
         return ResponseEntity.ok().body(this.playerService.convertPlayerToResponseUpdatePlayerDTO(updatedPlayer));
+    }
+    @GetMapping("/players/{id}")
+    @ApiMessage("Fetch a player")
+    public ResponseEntity<Player> fetchAPlayer(@PathVariable Long id) throws InvalidRequestException {
+        Optional<Player> player = this.playerService.getPlayerById(id);
+        if (!player.isPresent()) {
+            throw new InvalidRequestException("Player with id = " + id + " not found.");
+        }
+        return ResponseEntity.ok(player.get());
+    }
+
+    @GetMapping("/players")
+    @ApiMessage("Fetch all players")
+    public ResponseEntity<ResultPaginationDTO> fetchAllPlayers(
+        @Filter Specification<Player> spec,
+        Pageable pageable
+    ) {
+        return ResponseEntity.ok(this.playerService.fetchAllPlayers(spec, pageable));
+    }
+
+    @DeleteMapping("/players/{id}")
+    @ApiMessage("Delete a player")
+    public ResponseEntity<Void> deleteAPlayer(@PathVariable Long id) {
+        this.playerService.handleDeleteUser(id);
+        return ResponseEntity.ok(null);
     }
 
 }
--- epl-app/src/main/java/vn/duongvct/test/epl_app/service/PlayerService.java
+++ epl-app/src/main/java/vn/duongvct/test/epl_app/service/PlayerService.java
@@ -1,12 +1,18 @@
 package vn.duongvct.test.epl_app.service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import vn.duongvct.test.epl_app.domain.Player;
 import vn.duongvct.test.epl_app.domain.request.player.RequestCreatePlayerDTO;
 import vn.duongvct.test.epl_app.domain.request.player.RequestUpdatePlayerDTO;
+import vn.duongvct.test.epl_app.domain.response.ResultPaginationDTO;
 import vn.duongvct.test.epl_app.domain.response.player.ResponseCreatePlayerDTO;
 import vn.duongvct.test.epl_app.domain.response.player.ResponseUpdatePlayerDTO;
 import vn.duongvct.test.epl_app.repository.PlayerRepository;
@@ -77,6 +83,28 @@ public class PlayerService {
         playerDTO.setUpdatedAt(player.getUpdatedAt());
         return playerDTO;
 
+    }
+    public ResultPaginationDTO fetchAllPlayers(Specification<Player> spec, Pageable pageable) {
+        Page<Player> pagePlayer = this.playerRepository.findAll(pageable);
+        ResultPaginationDTO result = new ResultPaginationDTO();
+        ResultPaginationDTO.Meta meta = new ResultPaginationDTO.Meta();
+        meta.setPage(pageable.getPageNumber() + 1);
+        meta.setPageSize(pageable.getPageSize());
+        meta.setPages(pagePlayer.getTotalPages());
+        meta.setTotal(pagePlayer.getTotalElements());
+        result.setMeta(meta);
+        List<Player> list = pagePlayer.getContent().stream().collect(Collectors.toList());
+        result.setResult(list);
+        return result;
+    }
+    public void handleDeleteUser(Long id) {
+        //delete all club related
+        //TO-DO
+
+        //delete all player positions
+        //TO-DO
+
+        this.playerRepository.deleteById(id);
     }
 
 }
